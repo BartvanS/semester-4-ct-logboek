0a5770a990a0adf05d37e34adea679a498de3e34
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2021-02-23 14:17:21 +0100

    trackrobot start,

---
+++ tech/aandacht/index.js
@@ -0,0 +1,47 @@
+
+const express = require('express')
+const Readline = require('@serialport/parser-readline')
+const app = express()
+const webport = 3000
+const SerialPort = require('serialport')
+const port = new SerialPort('COM8', {
+	baudRate: 9600,
+	// autoOpen: false,
+});
+
+port.write('main screen turn on', function(err) {
+	if (err) {
+	  return console.log('Error on write: ', err.message)
+	}
+	console.log('message written')
+  })
+  
+  // Open errors will be emitted as an error event
+  port.on('error', function(err) {
+	console.log('Error: ', err.message)
+  })
+
+  port.on('readable', function () {
+	console.log('Data:', port.read())
+  })
+  
+  // Switches the port into "flowing mode"
+  port.on('data', function (data) {
+	console.log('Data:', data)
+  })
+  
+  // Pipe the data into another stream (like a parser or standard out)
+  const lineStream = port.pipe(new Readline())
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+app.post('/', (req, res) => {
+	console.log("wow")
+	res.send('wow')
+
+})
+
+app.listen(webport, () => {
+	console.log(`Example app listening at http://localhost:${webport}`)
+})
