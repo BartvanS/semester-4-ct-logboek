660c92c8cad4abe0e75df714264ff9f6a3b35a73
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2021-02-24 20:14:42 +0100

    trackingrobot serial communication basic setup

--- person tracking robot/trackingrobot/index.js
+++ person tracking robot/trackingrobot/index.js
@@ -5,8 +5,24 @@ const path = require('path')
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
-// const SerialPort = require('serialport');
-// const port = new SerialPort('/dev/ttyS8');
+const SerialPort = require('serialport')
+const port = new SerialPort('/dev/ttyS8', error => {
+	if (error) {
+		return console.log('Error: ', error.message)
+	  }
+});
+
+let msg = 'kaas\n';
+port.write(msg, function (err) {
+  if (err) {
+    return console.log('Error on write: ', err.message)
+  } 
+  console.log('written message:' + msg)
+});
+// Open errors will be emitted as an error event
+port.on('error', function (err) {
+  console.log('Error: ', err.message)
+});
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -15,19 +31,16 @@ app.get('/', (req, res) => {
 
 //sockets
 io.on('connection', socket => {
-	console.log("socket connected");
+  console.log('socket connected')
   socket.on('frontcam', msg => {
     console.log(msg)
     // io.emit('chat message', msg)
   })
-  socket.on("disconnect", (reason) => {
-    console.log("socket disconnected")
-  });
+  socket.on('disconnect', reason => {
+    console.log('socket disconnected')
+  })
 })
 
-// app.listen(appPort, () => {
-//   console.log(`Example app listening at http://localhost:${appPort}`)
-// })
 http.listen(appPort, () => {
-	console.log(`Example app listening at http://localhost:${appPort}`)
-  });
+  console.log(`Example app listening at http://localhost:${appPort}`)
+})
