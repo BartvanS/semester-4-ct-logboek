a4f963f1291b1c52b3aa9f1a9c7b2321df328aca
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2021-02-25 16:30:25 +0100

    small word changes index.js

--- person tracking robot/trackingrobot/index.js
+++ person tracking robot/trackingrobot/index.js
@@ -47,30 +47,30 @@ io.on('connection', socket => {
 function handleCalculations (data) {
   let left = data.left
   let right = data.right
-  left.convergence = { x: left.shoulder.x, y: left.elbow.y }
-  right.convergence = { x: right.shoulder.x, y: right.elbow.y }
+  left.convergenceElbow = { x: left.shoulder.x, y: left.elbow.y }
+  right.convergenceElbow = { x: right.shoulder.x, y: right.elbow.y }
   let sides = calculateSides(data)
   let angles = calculateDegreesObj(sides)
   return angles
 }
 
 function calculateSides (data) {
-	//todo: arms in t pose angle = 0
-	//both directions up and down are both positive between 0 and 90 because the abs funciton
+  // arms in t pose angle = 0
+  //both directions up and down are both positive between 0 and 90 because the abs funciton
   //Math.abs() if it needs to be always a positive number
   let left = data.left
-  let SoCoLeft = Math.abs(left.convergence.y - left.shoulder.y)
-  let CoElLeft = Math.abs(left.convergence.x - left.elbow.x)
-  let right = data.right
-  let SoCoRight = Math.abs(right.convergence.y - right.shoulder.y)
-  let CoElRight = Math.abs(right.elbow.x - right.convergence.x)
+  let ShCoLeft = Math.abs(left.convergenceElbow.y - left.shoulder.y); // Shoulder convergenceElbow
+  let CoElLeft = Math.abs(left.convergenceElbow.x - left.elbow.x); // convergenceElbow Elbow
+  let right = data.right;
+  let SoCoRight = Math.abs(right.convergenceElbow.y - right.shoulder.y);
+  let CoElRight = Math.abs(right.elbow.x - right.convergenceElbow.x);
   let sides = {
     left: {
-      SoCo: SoCoLeft,
+      ShCo: ShCoLeft,
       CoEl: CoElLeft
     },
     right: {
-      SoCo: SoCoRight,
+      ShCo: SoCoRight,
       CoEl: CoElRight
     }
   }
@@ -81,13 +81,13 @@ function calculateDegreesObj (data) {
   let right = data.right
   let angles = {
     left: {
-      shoulderX: calculateDegrees(left.SoCo, left.CoEl)
+      shoulderX: calculateDegrees(left.ShCo, left.CoEl)
       //shoulderX is for the frontal 2d side movement. z is for depth that might be added later on
       //   shoulderZ:,
       //   elbow: calculateDegrees(left.elbow.x, left.elbow.y)
     },
     right: {
-      shoulderX: calculateDegrees(right.SoCo, right.CoEl)
+      shoulderX: calculateDegrees(right.ShCo, right.CoEl)
       //   elbow: calculateDegrees(right.elbow.x, right.elbow.y)
     }
   }
